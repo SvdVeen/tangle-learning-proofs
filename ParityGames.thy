2d1c9efea523a34b3658f7cb9405072911437334
Peter Lammich <plammich@gmail.com>
2022-12-16 13:50:49 +0100

    meeting 16 dec

--- ParityGames.thy
+++ ParityGames.thy
@@ -40,4 +40,54 @@ text \<open>Just a quick test that I did this right: V should have no overlap wi
 lemma \<A>V\<^sub>0_\<A>V\<^sub>1_no_overlap: "\<forall>v\<in>(\<A>V\<^sub>0 a). v \<notin> (\<A>V\<^sub>1 a)"
   unfolding \<A>V\<^sub>0_def \<A>V\<^sub>1_def
   by auto
+  
+  
+locale arena_defs =
+  fixes E :: "'v dgraph"
+  fixes V\<^sub>0 :: "'v set"
+  fixes prio :: "'v \<Rightarrow> nat"
+begin  
+  definition V where "V = fst`E \<union> snd`E"
+  definition V\<^sub>1 where "V\<^sub>1 = V-V\<^sub>0"
+
+end
+
+locale arena = arena_defs E V\<^sub>0 
+  for  
+    E :: "'v dgraph"
+  and V\<^sub>0 :: "'v set"  
+  + 
+  assumes V0_ss: "V\<^sub>0 \<subseteq> V"
+  assumes succ: "E``{v}\<noteq>{}"
+  
+  assumes fin: "finite (UNIV::'v set)"
+  
+  
+begin
+  
+  definition "connected v v' \<longleftrightarrow> (v,v')\<in>E\<^sup>*"
+
+  
+  lemma conn: "v\<in>V \<Longrightarrow> connected v v' \<Longrightarrow> v'\<in>V" sorry
+  
+
+end
+  
+lemma 
+  assumes "arena (\<A>E \<A>) (\<A>V\<^sub>0 \<A>)"  
+  assumes "v\<in>\<A>V \<A>" "(v,v')\<in>(\<A>E \<A>)\<^sup>*"  
+  shows "v'\<in>\<A>V \<A>"
+proof -
+  interpret arena "\<A>E \<A>" "\<A>V\<^sub>0 \<A>" by fact
+
+  thm conn
+  from assms conn show ?thesis
+    unfolding V_def \<A>V_def ParityGames.V_def connected_def by blast
+qed  
+
+  
+
+
+
+  
 end
